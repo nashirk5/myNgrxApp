a96512a50657ced690faa6d26546a4b0ce8473ff
nashir <nashirk5@gmail.com.com>
2024-02-25 16:36:31 +0530

    Created Post User module and setup the layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './_core/layout/layout.component';
+import { PageNotFoundComponent } from './_core/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'post',
+    component: LayoutComponent,
+    loadChildren: () => import('./post/post.module').then((m) => m.PostModule),
+  },
+  {
+    path: 'user',
+    component: LayoutComponent,
+    loadChildren: () => import('./user/user.module').then((m) => m.UserModule),
+  },
+  { path: '', redirectTo: '/post', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './_core/header/header.component';
+import { FooterComponent } from './_core/footer/footer.component';
+import { PageNotFoundComponent } from './_core/page-not-found/page-not-found.component';
+import { LayoutComponent } from './_core/layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PageNotFoundComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
